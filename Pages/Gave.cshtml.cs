c8e2464b7d0f050688629585c8e5f7dfc274bf9c
Simon Christiansen <simon@letusdoit.dk>
2022-06-20 21:23:25 +0200

    First shot

---
+++ Pages/Gave.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tillykke.Pages
+{
+    public class GaveModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
